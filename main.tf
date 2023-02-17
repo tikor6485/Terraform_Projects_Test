8632338df91e577c5a36bb24a7a9d1bcece3a059
tikor6485 <tikor6485@yahoo.com>
2023-02-17 16:34:26 +0100

    Chenge main to test

--- main.tf
+++ main.tf
@@ -9,7 +9,16 @@ resource "aws_instance" "TirdadKor" {
   }
 }
 
+resource "aws_instance" "TirdadKor2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
 
+  tags = {
+    Name       = var.machine_name
+    Created_By = "Terraform-Tirdad"
+  }
+}
 # resource "aws_instance" "TirdadKor-2" {
 #   ami           = "ami-0aa7d40eeae50c9a9"
 #   instance_type = "t2.micro"
